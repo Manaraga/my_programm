a94fb4d92d48a68a4625caed30d53001bbe05285
Alex <manaraga2008@yandex.ru>
2012-01-04 16:35:46 +0300

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,4 @@
+# Ruby on Rails: пример приложения
+
+Это пример web-приложения by Alex Korzun
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '00d802dcadea41f3dc0c2deead542c2277f9d0ac9691ef7aaeac4efd5ea8c377aa3824a6a48a45800e4b774f040e8b8d079873cddcd55cc8c9297b1f4d8f1272'

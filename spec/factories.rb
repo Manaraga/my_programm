60e7526dc00ce15827dd33e7ed16aebd33f6be5c
Alex <manaraga2008@yandex.ru>
2012-01-09 00:00:21 +0300

    User model with passwords, and user show page

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                  "Alex Korzun"
+  user.email                 "korzun.alex@mail.ru"
+  user.password              "foobar"
+  user.password_confirmation "foobar"
+end
